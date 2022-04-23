4fba9a5e116f95afa21ed970bb8e88429b10ad69
Wijayaac <wijayaac25@gmail.com>
2022-04-23 22:30:40 +0800

    Fix : skeleton components

---
+++ src/components/Form/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import style from "./style.module.css";
+
+function Form() {
+  return (
+    <div>
+      Form
+      <div className={style.wrapper}>
+        <button>Button</button>
+      </div>
+    </div>
+  );
+}
+
+export default Form;
